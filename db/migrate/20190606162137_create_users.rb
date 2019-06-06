579e85a6e0a7fd9f6c73d009413f7bb657458e96
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-06 21:03:26 +0000

    Automatically backed up by Learn

---
+++ db/migrate/20190606162137_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |el|
+      el.string :username
+      el.string :password 
+      el.float :balance
+    end 
+  end
+end
