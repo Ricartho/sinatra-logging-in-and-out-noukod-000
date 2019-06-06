0f8ed6a741b98bf12944c588712aa4d9b9e5b44c
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-06 20:52:46 +0000

    savefinal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    if !Helpers.is_logged_in?(session)
+      erb  :error
+    else
+      erb :account
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
